5510927799f5665da94e54b1425690b03b000012
kathrynhoule <kathryn.houle@torontomu.ca>
2024-09-11 19:19:18 -0400

    adds react-dom

--- src/components/ticTacToe.jsx
+++ src/components/ticTacToe.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { useState } from "react";
 import Board from "./board";
 
 function TicTacToe() {
+     const [tiles, setTiles] = useState(Array(9).fill(null));
      return (
           <div>
                <h1>Tic Tac Toe</h1>
-               <Board />
+               <Board tiles={tiles} />
           </div>
      );
 }
--- src/components/tile.jsx
+++ src/components/tile.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./tile.css";
 
-function Tile({className}) {
+function Tile({className, value }) {
      return (
-          <div className={`tile ${className}`}>X</div>
+          <div className={`tile ${className}`}>{value}</div>
      );
 }
 
