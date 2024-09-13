8dcbaf790c29d2ce4ad0c0d926784f32b66524ee
kathrynhoule <kathryn.houle@torontomu.ca>
2024-09-13 13:11:33 -0400

    adds array of winning combinations

--- src/components/ticTacToe.jsx
+++ src/components/ticTacToe.jsx
@@ -4,6 +4,22 @@ import Board from "./board";
 const PLAYER_X = "X";
 const PLAYER_O = "O";
 
+const winnerCombinations = [
+     // rows
+     {combo:[0,1,2], strikeClass: "strike-row-1"},
+     {combo:[3,4,5], strikeClass: "strike-row-2"},
+     {combo:[6,7,8], strikeClass: "strike-row-3"},
+
+     // columns
+     {combo:[0,3,6], strikeClass: "strike-column-1"},
+     {combo:[1,4,7], strikeClass: "strike-column-2"},
+     {combo:[2,5,8], strikeClass: "strike-column-3"},
+
+     // diagonals
+     {combo:[0,4,8], strikeClass: "strike-diagonal-1"},
+     {combo:[2,4,6], strikeClass: "strike-diagonal-2"},
+]
+
 function checkWinner() {
 
 };
